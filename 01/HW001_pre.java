28fd24fe5048bc07863418bb84572ddd4ecf4467
조진호 <68462676+JoJinho333@users.noreply.github.com>
2023-03-22 10:26:07 +0900

    Add files via upload

---
+++ 01/HW001_pre.java
@@ -0,0 +1,89 @@
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HW001_pre {
+
+    public static void main(String[] args) {
+        //프로그램 시작
+        //1. 이름 등록
+
+        ArrayList<String> nameList = new ArrayList<>(); //이름등록부
+
+        Scanner scName=new Scanner(System.in);//입력받을 스캐너 객체 (이름)
+        Scanner scMenu=new Scanner(System.in);//입력받을 스캐너 객체 (메뉴)
+
+        String name;//문자열 변수
+        int choice;// 메뉴 입력 변수
+        boolean flag=true; //프로그램 동작 여부
+        int count = 0;
+
+
+        while(flag){
+
+            System.out.println("=============");
+            System.out.println("1.입력");
+            System.out.println("2.출력");
+            System.out.println("3.삭제");
+            System.out.println("4.종료");
+            System.out.println("=============");
+            System.out.println("메뉴 입력 :");
+            choice= scMenu.nextInt();
+
+            switch(choice){
+
+                case 1: //입력
+                    System.out.println("등록할 이름을 입력 : ");
+                    name=scName.nextLine();
+
+                    nameList.add(name);
+                    count++;
+
+                    break;
+
+                case 2: //출력
+
+                    for(int i=0; i< nameList.size(); i++){
+
+                        System.out.print(i);
+                        System.out.println(". "+nameList.get(i));
+
+                    }
+
+                    break;
+
+                case 3:
+
+                    System.out.println("삭제할 이름을 입력 : ");
+                    name=scName.nextLine();
+
+                    for(int i=0; i< nameList.size(); i++){
+
+                        if(name.equals(nameList.get(i))){//같은 문자열이 있을경우
+                            nameList.remove(i);
+                        }
+
+                    }
+                    
+                    if (nameList.size() != count) {
+
+                        System.out.println("삭제완료!");
+                        break;
+                        
+                    } else {
+                        System.out.println("말씀하신 이름이 없습니다!");
+                    }
+                    
+
+                case 4: //종료
+                    flag=false;
+                    System.out.println("******프로그램 종료");
+                    break;
+
+            }
+
+        }
+
+    }
+
+}
