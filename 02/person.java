acfeb136a220809176448c0c98a0653635385dc7
조진호 <68462676+JoJinho333@users.noreply.github.com>
2023-03-24 18:00:08 +0900

    Add files via upload

---
+++ 02/person.java
@@ -0,0 +1,31 @@
+package vo;
+
+public class person {
+
+    private String name;
+    private String ssn;
+
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public String getSsn(){
+
+        return ssn;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setSsn(String ssn){
+        this.ssn=ssn;
+    }
+
+
+}
+
+
