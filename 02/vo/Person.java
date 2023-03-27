d24d63103041e132594f652773815962ca6dc140
조진호 <68462676+JoJinho333@users.noreply.github.com>
2023-03-27 17:56:56 +0900

    Add files via upload

--- 02/vo/Person.java
+++ 02/vo/Person.java
@@ -1,24 +1,24 @@
 package vo;
 
+
+
 public class Person {
 
-
-    //필드
     private String name;
     private String ssn;
 
-    //생성자 - 일종의 메서드, 생성할 때 단 한 번 호출됨
+
     public Person(String name, String ssn){
         this.name=name;
         this.ssn=ssn;
     }
 
-    public Person(){
-
+    public Person(String ssn){
+        this.ssn=ssn;
     }
 
-    //getter
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
@@ -26,7 +26,6 @@ public class Person {
         return ssn;
     }
 
-    //setter
     public void setName(String name){
         this.name=name;
     }
@@ -37,7 +36,10 @@ public class Person {
 
     @Override
     public String toString(){
-        return "이름 : "+this.name+", 주민번호 : "+this.ssn;
+        return "이름 : "+this.name +", 주민번호 : "+this.ssn;
     }
 
+
 }
+
+
