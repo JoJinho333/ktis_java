4bbcdac27fdb22b45449b0525154707a929fc080
조진호 <68462676+JoJinho333@users.noreply.github.com>
2023-04-03 17:13:10 +0900

    homework01 추가

---
+++ 02/HOMEWORK01/src/Main.java
@@ -0,0 +1,16 @@
+import ui.PersonUI;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        String s = "Hello world";
+        String newString = s.replace(s, "kim");
+        System.out.println(newString);
+
+        System.out.println(Person person);
+
+         */
+        new PersonUI();
+    }
+}
